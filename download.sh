40680b7ebaccee31b4b3600318a49fac8bef029d
shb9019 <saihemanth9019@gmail.com>
2017-10-30 18:07:32 +0530

    Download movies through Geckodriver

---
+++ download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -P /home/sai9019/Downloads/Series/$2 $1
--- scraper.py
+++ scraper.py
@@ -1,6 +1,8 @@
 import urllib2
 import sys
 import time
+import shlex
+import subprocess
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
@@ -11,26 +13,28 @@ from selenium.common.exceptions import TimeoutException
 
 
 search_input = sys.argv[1]
+stream_method = sys.argv[2]
+
 search_tokens = search_input.split(' ')
 search_string = ''
 for s in search_tokens:
 	search_string += s + '+'
 search_string = search_string.rstrip('+')
 
-print "Searching..."
+print "Searching for " + search_input + "..."
 
 path_name = 'https://fmovies.se/search?keyword=' + search_string
 hdr = {'User-Agent': 'Mozilla/5.0'}
 req = urllib2.Request(path_name,headers = hdr)
 page = urllib2.urlopen(req)
 soup = BeautifulSoup(page,'html.parser')
-browser = webdriver.PhantomJS('./phantomjs')
+browser = webdriver.Firefox(executable_path = '/home/sai9019/FmoviesDownloader/geckodriver')
 movie_box = soup.find('a',attrs = {'class':'name'})
 movie_name = movie_box.text.strip()
 
 if movie_name.lower() == search_input.lower() :
-	print "Requested Movie/Series is Available"
-	print "Proceeding to Download..."
+	print "Requested Movie/Series is Available\n\n"
+	print "Proceeding to Obtain Links..."
 	path_name = 'https://fmovies.se' + movie_box.get('href')
 	req = urllib2.Request(path_name,headers = hdr)
 	page = urllib2.urlopen(req)
@@ -44,35 +48,47 @@ if movie_name.lower() == search_input.lower() :
 	for i in range(episodes_number):
 		episode_link = 'https://fmovies.se' + episodes_box[i].get('href')
 		episode_links.append(episode_link)
-	
-	print 'Preparing to Download'
 
+	if episodes_number != 1:
+		print "Number of Episodes: " + episodes_number + '\n'
+
+	print 'Preparing to Download'
+	
+	i = 1
+	links = []
 	for episode_link in episode_links:
+		if episodes_number != 1:
+			print 'Downloading episode ' + str(i)
 		browser.get(episode_link)
 		element = WebDriverWait(browser,10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='player']")))
-		print "Page loaded... Clicking now"
-		browser.save_screenshot('beforeclick.png')
-		#try:
-		print browser.find_element_by_css_selector("div.cover").text
-		#except WebDriverException:
-		#	print "Element not clickable"			
-		#WebDriverWait(browser,120).until(EC.presence_of_element_located((By.XPATH, "//*[@id='jw']/div[2]/video")))
-		#print "Waiting for page to load..."
-		#print browser.find_element_by_xpath("//*[@id='jw']/div[2]/video").get_attribute('src')		
+		if episodes_number != 1:
+			print "Obtained Episode. Proceeding to get link"
+		else:
+			print "Obtained Movie. Proceeding to get link"
+		clickable = browser.find_element_by_id("player")
+		clickable.find_element_by_xpath('.//*').click()
+		time.sleep(5)
+		link = browser.find_element_by_xpath('//*[@id="jw"]/div[2]/video').get_attribute('src')
+		links.append(link)
+		i += 1
+		print "Obtained Link\n"
 
 	browser.close()
-	browser.quit()	
+	links_string = ''
+	i = 1
+	for link in links:
+		if episodes_number != 1:
+			print 'Downloading episode' + str(i) + 'to Local'
+		else:
+			print 'Downloading movie to Local'
+		subprocess.call (shlex.split('bash ./download.sh ' + link + ' "' + search_input + '"'))
+		i += 1
+
+	print 'Downloaded all files'
+	browser.quit()
 
 else :
 	print "Unavailable \nDid you mean? \n"	
 	movie_suggestions = soup.find_all('a',attrs = {'class':'name'})
-	for i in range(0,10) :
-		print movie_suggestions[i].text.strip()
-
-#for movie_name_box in movie_names_box:
-	#movie_name = movie_name_box.text.strip()
-	#movie_link = movie_name_box.get('href')
-	#print movie_name
-	#print 'https://fmovies.se' + movie_link
-	#print '\n'
-
+	for movie in movie_suggestions:
+		print movie.text.strip()
